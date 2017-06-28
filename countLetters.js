07d90b46fb38ec7a532fa2b4cad860e1969ed24f
Helen <howareyouhelen@hotmail.com>
2017-06-28 19:09:15 +0000

    put object in function

---
+++ countLetters.js
@@ -0,0 +1,10 @@
+function countLetters() {
+  var howmanyletters = {
+  specialLetters: ["l: 1", "i: 2", "g: 1", "h: 4", "t: 2", "o: 2", "u: 2", "s: 2", "e: 3", "n: 1"]
+  }
+  return howmanyletters
+}
+
+ console.log(countLetters.howmanyletters)
+
+
